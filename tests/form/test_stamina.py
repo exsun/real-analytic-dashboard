7b2e908f771302a66f9e1930795a211ec464a843
actin <space@actinhealth.ir>
2025-01-06 21:15:57 +0330

    update test

--- tests/form/test_stamina.py
+++ tests/form/test_stamina.py
@@ -7,8 +7,8 @@ def test_number_input():
     """A user number input, then clicks save"""
     at = AppTest.from_file("pages/form/stamina.py").run()
     
-    at.number_input(key="distance_input").set_value(45).run()
-    assert at.number_input(key="distance_input").value == 45
+    at.number_input(key="distance_6min").set_value(45).run()
+    assert at.number_input(key="distance_6min").value == 45
 
 if __name__ == "__main__":
     test_number_input()
